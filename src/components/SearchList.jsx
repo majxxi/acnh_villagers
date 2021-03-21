bd96e300102cf84a85c68a39b3794437426642fc
majick <majicktadepa@gmail.com>
2021-03-21 10:28:51 -0700

    remove logging statements

--- src/components/SearchList.jsx
+++ src/components/SearchList.jsx
@@ -2,13 +2,10 @@ import React, { useState, useEffect } from 'react';
 
 import AcnhApi from '../Api';
 import SearchCard from './SearchCard';
-import { Typography } from '@material-ui/core';
-
 
 function SearchList({search}){
   const [villagers, setVillagers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  console.log(search)
 
   useEffect(function searchVillagersOnRender() {
     async function searchVillager(search){
@@ -17,14 +14,13 @@ function SearchList({search}){
         const response = await AcnhApi.getVillager(search); //search should be here
         setVillagers(Object.entries(response));
         setIsLoading(false);
-        console.log(villagers)
       } catch (error) {
         setVillagers([]);
         setIsLoading(false);
       }
     };
     searchVillager(search);
-  }, [search])
+  }, [search]);
   
 
   return (
